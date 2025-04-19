dfa715d86750b5b89924613ef4ab7b32775024d3
wirapat.g <wirapat@sirisoft.co.th>
2025-04-19 16:32:31 +0700

    feat: update example data api-docs

--- src/dto/get-encrypt-data.dto.ts
+++ src/dto/get-encrypt-data.dto.ts
@@ -1,8 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class GetEncryptDataDto {
   @IsString()
   @IsNotEmpty()
   @Length(0, 2000)
+  @ApiProperty({ example: 'data information' })
   payload: string;
 }
